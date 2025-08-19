023590c2f306ea316a14ad54c924c6db179a8223
edgar hugo <022200901g@uandina.edu.pe>
2025-08-19 18:02:48 -0500

    version3

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,8 +18,56 @@ class HomeScreen extends StatelessWidget{
 
   Widget _plantsCard(BuildContext context){
     return Padding(
-      padding: const EdgeInsets.all(0.0)),
-      child: SizedBox)
+      padding: const EdgeInsets.all(0.0),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    '',
+                    fit: BoxFit.cover,
+                  ),
+                )),
+              SizedBox(
+                width: 26,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget> [
+                  Text('planta',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Quicksand',
+                      color: Colors.brown,
+                      fontWeight: FontWeight.w500)), 
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Container(
+                    height: 1,
+                    width: 95,
+                    color: Colors.orange,
+                  ),
+                  Text('Edgar hugo',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand')),   
+                  SizedBox(
+                    height: 4,
+                  )   
+                ],
+              )
+            ]
+            ),
+        ),
+          ),      
+      );
   }
 
 }
